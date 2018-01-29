8b89247f9d5e047355dfb6516f9b098311cfb46e
Shawn070 <307882919@qq.com>
2018-01-29 18:45:12 +0800

    常规更新

--- quadratic.py
+++ quadratic.py
@@ -4,7 +4,15 @@ def main():
     print("This program find the rea solutions to a quadratic\n")
     a, b, c = eval(input("Please enter the coefficients(a, b, c):"))
     delta = b * b - 4 * a * c
-    if delta >= 0:
+    if a == 0:
+        x = -b / c
+        print("\nThere is an solution", x)
+    elif delta < 0:
+        print("\nThe equation has no real roots!")
+    elif delta ==0:
+        x = -b /(2 * a)
+        print("\nThere is a double root at", x)
+    
         discRoot = math.sqrt(delta)
         root1 = (-b + discRoot) / (2 * a)
         root2 = (-b - discRoot) / (2 * a)
---
+++ try_except.py
@@ -0,0 +1,15 @@
+def main():
+    try:
+        number1, number2 = eval(input("Enter two numbers, separated by a comma:"))
+        result = number1 / number2
+    except ZeroDivisionError:
+        print("Division by zero!")
+    except SyntaxError:
+        printf("A comma may be missing in the input")
+    except:
+        print("Something wrong in the input")
+    else:
+        print("No exceptions, the result is", result)
+    finally:
+        print("executing the final clause")
+main()
