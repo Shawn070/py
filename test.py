28c8b9caf5c2d8c1860327cd8785e4dda59d2f11
Shawn070 <307882919@qq.com>
2018-02-02 10:59:07 +0800

    添加

---
+++ test.py
@@ -0,0 +1,21 @@
+def main():
+    # 用户输入文件名
+    f1 = input("Enter a souce file:").strip()
+    f2 = input("Enter a souce file:").strip()
+
+    # 打开文件
+    infile = open(f1, "r")
+    outfile = open(f2, "w")
+
+    # 拷贝数据
+    countLines = countChars = 0
+    for line in infile:
+        countLines += 1
+        countChars += len(line)
+        outfile.write(line)
+    print(countLines, "lines and", countChars, "chare copied")
+
+    infile.close()
+    outfile.close()
+    print()
+main()
